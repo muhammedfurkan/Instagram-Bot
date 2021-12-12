cdfce5f9f46471eae20a23efeb29d933fcfa279a
muhammedfurkan <byazade@yandex.com>
2021-12-12 22:20:56 +0300

    update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 
+from pyrogram import Client, idle
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,8 +21,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 from pyrogram.raw import functions, types
-from pyrogram import Client, idle
 from pyromod import listen
+
 from config import Config
 
 STATUS = Config.STATUS
@@ -50,66 +51,6 @@ if Config.INSTA_SESSIONFILE_ID:
     bot.run(main())
 
 bot.start()
-bot.send(
-    functions.bots.SetBotCommands(
-        commands=[
-            types.BotCommand(command="start", description="Check if bot alive"),
-            types.BotCommand(command="help", description="How to use"),
-            types.BotCommand(
-                command="login", description="login into your Instagram account"
-            ),
-            types.BotCommand(command="logout", description="Logout from Instagram"),
-            types.BotCommand(
-                command="account", description="Shows details of logged in account"
-            ),
-            types.BotCommand(
-                command="posts", description="Download all posts from given username"
-            ),
-            types.BotCommand(command="feed", description="Download posts in your feed"),
-            types.BotCommand(
-                command="igtv", description="Download IGTV videos of given username"
-            ),
-            types.BotCommand(
-                command="saved",
-                description="Download specified number of posts from your saved posts ",
-            ),
-            types.BotCommand(
-                command="story", description="Download stories of given username"
-            ),
-            types.BotCommand(
-                command="stories",
-                description="Downloads stories off all your followees",
-            ),
-            types.BotCommand(
-                command="followers",
-                description="Sends a list of followers of given username",
-            ),
-            types.BotCommand(
-                command="followees",
-                description="Sends a list followees of given username",
-            ),
-            types.BotCommand(
-                command="fans",
-                description="Get a list of followees who is following back.",
-            ),
-            types.BotCommand(
-                command="notfollowing",
-                description="Get a list of followees who is not following back.",
-            ),
-            types.BotCommand(
-                command="tagged",
-                description="Download all posts tagged with given username",
-            ),
-            types.BotCommand(
-                command="highlights",
-                description="Downloads highlights from given username",
-            ),
-            types.BotCommand(
-                command="restart", description="Stop all processes and restart bot"
-            ),
-        ]
-    )
-)
 
 idle()
 bot.stop()
